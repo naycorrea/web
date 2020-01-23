1507ea980aa0cd3c77e9ab7ec991d4be382ff575
nayaracorrea <43212442+nayaracorrea@users.noreply.github.com>
2020-01-23 09:06:17 -0300

    upd

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 import './global.css';
 import './App.css';
-// import './Sidebar.css';
-// import './Main.css';
+import './Sidebar.css';
+import './Main.css';
 
 function App() {
     return (
@@ -11,33 +11,82 @@ function App() {
             <aside>
                 <strong>Cadastrar</strong>
                 <form>
-                    <div class="input-block">
+                    <div className="input-block">
                         <label htmlFor="github_username">Usuário do Github</label>
                         <input name="github_username" id="github_username" required />
                     </div>
 
-                    <div class="input-block">
+                    <div className="input-block">
                         <label htmlFor="techs">Tecnologias</label>
                         <input name="techs" id="techs" required />
                     </div>
 
                     <div className="input-group">
-                        <div class="input-block">
+                        <div className="input-block">
                             <label htmlFor="latitude">Latitude</label>
                             <input name="latitude" id="latitude" required />
                         </div>
 
-                        <div class="input-block">
+                        <div className="input-block">
                             <label htmlFor="longitude">Longitude</label>
                             <input name="longitude" id="longitude" required />
                         </div>
                     </div>
 
-
                     <button type="submit">Salvar</button>
                 </form>
             </aside>
-            <main></main>
+            <main>
+                <ul>
+                    <li className="dev-item">
+                        <header>
+                            <img src="https://avatars3.githubusercontent.com/u/43472806?s=460&v=4" alt="Nayara"/>
+                            <div className="user-info">
+                                <strong>Nayara Corrêa</strong>
+                                <span>ReactJS, React Native, Node.JS</span>
+                            </div>
+                        </header>
+                        <p>Estudante de Análise de Sistemas e desenvolvera de sistemas em uma StartUp voltada para a área da saúde.</p>
+                        <a href="https://github.com/naycorrea">Acessar perfil no Github</a>
+                    </li>
+
+                    <li className="dev-item">
+                        <header>
+                            <img src="https://avatars3.githubusercontent.com/u/43472806?s=460&v=4" alt="Nayara"/>
+                            <div className="user-info">
+                                <strong>Nayara Corrêa</strong>
+                                <span>ReactJS, React Native, Node.JS</span>
+                            </div>
+                        </header>
+                        <p>Estudante de Análise de Sistemas e desenvolvera de sistemas em uma StartUp voltada para a área da saúde.</p>
+                        <a href="https://github.com/naycorrea">Acessar perfil no Github</a>
+                    </li>
+
+                    <li className="dev-item">
+                        <header>
+                            <img src="https://avatars3.githubusercontent.com/u/43472806?s=460&v=4" alt="Nayara"/>
+                            <div className="user-info">
+                                <strong>Nayara Corrêa</strong>
+                                <span>ReactJS, React Native, Node.JS</span>
+                            </div>
+                        </header>
+                        <p>Estudante de Análise de Sistemas e desenvolvera de sistemas em uma StartUp voltada para a área da saúde.</p>
+                        <a href="https://github.com/naycorrea">Acessar perfil no Github</a>
+                    </li>
+
+                    <li className="dev-item">
+                        <header>
+                            <img src="https://avatars3.githubusercontent.com/u/43472806?s=460&v=4" alt="Nayara"/>
+                            <div className="user-info">
+                                <strong>Nayara Corrêa</strong>
+                                <span>ReactJS, React Native, Node.JS</span>
+                            </div>
+                        </header>
+                        <p>Estudante de Análise de Sistemas e desenvolvera de sistemas em uma StartUp voltada para a área da saúde.</p>
+                        <a href="https://github.com/naycorrea">Acessar perfil no Github</a>
+                    </li>
+                </ul>
+            </main>
         </div>
     )
 }
